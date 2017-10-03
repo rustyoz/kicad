4897f053244c9a0a3c0aff3db672e7b573a1216c
Russell Oliver <roliver8143@gmail.com>
2017-10-03 21:30:23 +1100

    Eeschema Eagle Import: Code clean-up and formatting

--- pcbnew/librairi.cpp
+++ pcbnew/librairi.cpp
@@ -455,6 +455,7 @@ wxString PCB_BASE_EDIT_FRAME::CreateNewLibrary(const wxString& aLibName )
     if( aLibName.IsEmpty() )
     {
         DIALOG_SELECT_PRETTY_LIB dlg( this, initialPath );
+
         if( dlg.ShowModal() != wxID_OK )
             return wxEmptyString;
 
